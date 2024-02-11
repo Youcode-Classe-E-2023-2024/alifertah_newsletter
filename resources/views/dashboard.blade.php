b06abb22022734f5af7f35b116ff8b7aa98f9736
alifertah <alifertah42@gmail.com>
2024-02-11 15:17:38 +0100

    feat : table in dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,19 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>register</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+        integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" 
+            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+
+    <title>Dashboard</title>
 </head>
-<body>
-    <h1>Hello </h1>
-    <a
-                            role="menuitem"
-                            class="block p-2 text-sm text-gray-400 transition-colors duration-200 rounded-md dark:text-gray-400 dark:hover:text-light hover:text-gray-700"
-                        >
-                            <form action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <button type="submit">Logout</button>
-                            </form>
-                        </a>
+<body class="">
+<form action="{{ route('logout') }}" method="POST">
+    @csrf
+    <button type="submit">Logout</button>
+</form>
+<table class="min-w-full ">
+    <thead>
+        <tr>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
+        </tr>
+        <tr >
+            <td class="px-6 py-4 whitespace-nowrap">Test</td>
+            <td class="px-6 py-4 whitespace-nowrap">Test</td>
+            <td class="px-6 py-4 whitespace-nowrap">
+            <label for="underline_select" class="sr-only">Underline select</label>
+            <select id="underline_select" class="block py-2.5 px-0 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                <option selected>Choose a country</option>
+                <option value="US">Admin</option>
+                <option value="CA">Moderator</option>
+            </select>
+            </td>
+        </tr>
+    </thead>
+    <tbody id="users" class="bg-white divide-y divide-gray-200">
+    </tbody>
+</table>
+
 </body>
 </html>
